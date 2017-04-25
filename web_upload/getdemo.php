28d848555cda1b637af63c13f4d5737211bf6e1f
Kruzya <webmaster@kruzefag.ru>
2017-04-25 22:18:07 +0400

    Updated GetDemo script for new DB class

--- web_upload/getdemo.php
+++ web_upload/getdemo.php
@@ -27,34 +27,27 @@
 
 require_once("init.php");
 
-if(!isset($_GET['id']) || !isset($_GET['type']))
-  die('No id or type parameter.');
+if (!isset($_GET['id']) || !isset($_GET['type']))
+    die('Нет ID демо-записи/типа.');
 
-if(strcasecmp($_GET['type'], "U") != 0 && strcasecmp($_GET['type'], "B") != 0 && strcasecmp($_GET['type'], "S") != 0)
-  die('Bad type');
+if (strcasecmp($_GET['type'], "U") != 0 && strcasecmp($_GET['type'], "B") != 0 && strcasecmp($_GET['type'], "S") != 0)
+    die('Плохой тип демо-записи.');
 
-$id = (int)$_GET['id'];
+$demo = $GLOBALS['db']->getRow("SELECT filename, origname FROM `:prefix:demos` WHERE demtype=? AND demid=?;", array($_GET['type'], intval($id)));
 
-$demo = $GLOBALS['db']->GetRow("SELECT filename, origname FROM `".DB_PREFIX."_demos` WHERE demtype=? AND demid=?;", array($_GET['type'], $id));
-//Official Fix: https://code.google.com/p/sourcebans/source/detail?r=165
-if(!$demo)
-{
-  die('Demo not found.');
+if (!$demo)
+    die('Демо-запись не найдена.');
+
+if ((!in_array($demo['filename'], scandir(SB_DEMOS)) || !file_exists(SB_DEMOS . "/" . $demo['filename'])) && $_GET['type'] != "U")
+    die('Файл не найден.');
+
+if ($_GET['type'] != "U"){
+    $demo['filename'] = basename($demo['filename']);
+    header('Content-type: application/force-download');
+    header('Content-Transfer-Encoding: Binary');
+    header('Content-disposition: attachment; filename="' . $demo['origname'] . '"');
+    header("Content-Length: " . filesize(SB_DEMOS . "/" . $demo['filename']));
+    readfile(SB_DEMOS . "/" . $demo['filename']);
+} else {
+    header('Location: '.$demo['origname'], true, 301);
 }
-
-if((!in_array($demo['filename'], scandir(SB_DEMOS)) || !file_exists(SB_DEMOS . "/" . $demo['filename'])) && $_GET['type'] != "U")
-{
-  die('File not found.');
-}
-
-if($_GET['type'] != "U"){
-$demo['filename'] = basename($demo['filename']);
-header('Content-type: application/force-download');
-header('Content-Transfer-Encoding: Binary');
-header('Content-disposition: attachment; filename="' . $demo['origname'] . '"');
-header("Content-Length: " . filesize(SB_DEMOS . "/" . $demo['filename']));
-readfile(SB_DEMOS . "/" . $demo['filename']);
-}else{
-header( 'Location: '.$demo['origname'].'', true, 301 );
-}
-?>
