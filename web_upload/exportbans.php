3f6a86ccc9b6668c80dd7c5bc554ebf31aba0ccb
Kruzya <webmaster@kruzefag.ru>
2017-04-25 22:14:17 +0400

    Updated ExportBans script for new DB class

--- web_upload/exportbans.php
+++ web_upload/exportbans.php
@@ -1,31 +1,23 @@
 <?php
-	include_once("init.php");
-	$exportpublic = (isset($GLOBALS['config']['config.exportpublic']) && $GLOBALS['config']['config.exportpublic'] == "1");
-	if(!$userbank->HasAccess(ADMIN_OWNER) && !$exportpublic) {
-		echo "Don't have access to this feature.";
-	}
-	else if(!isset($_GET['type'])) {
-		echo "You have to specify the type. Only follow links!";
-	}
-	else {
-		if($_GET['type'] == 'steam') {
-			header('Content-Type: application/x-httpd-php php');
-			header('Content-Disposition: attachment; filename="banned_user.cfg"');
-			$bans = $GLOBALS['db']->Execute("SELECT authid FROM `".DB_PREFIX."_bans` WHERE length = '0' AND RemoveType IS NULL AND type = '0'");
-			$num = $bans->RecordCount();
-			for($x=0;$x<$num;$x++) {
-				print("banid 0 ".$bans->fields['authid']."\r\n");
-				$bans->MoveNext();
-			}
-		} elseif($_GET['type'] == 'ip') {
-			header('Content-Type: application/x-httpd-php php');
-			header('Content-Disposition: attachment; filename="banned_ip.cfg"');
-			$bans = $GLOBALS['db']->Execute("SELECT ip FROM `".DB_PREFIX."_bans` WHERE length = '0' AND RemoveType IS NULL AND type = '1'");
-			$num = $bans->RecordCount();
-			for($x=0;$x<$num;$x++) {
-				print("addip 0 ".$bans->fields['ip']."\r\n");
-				$bans->MoveNext();
-			}
-		}
-	}
-?>
+
+include_once("init.php");
+
+$exportpublic = (isset($GLOBALS['config']['config.exportpublic']) && $GLOBALS['config']['config.exportpublic'] == "1");
+
+if(!$userbank->HasAccess(ADMIN_OWNER) && !$exportpublic)
+    die("У Вас нет доступа к данной функции.");
+
+if(!isset($_GET['type']))
+    die("Не передан тип банов. Используйте только ссылки внутри веб-панели!");
+
+$type   = (($_GET['type'] == "steam") ? "0" : (($_GET['type'] == "ip") ? "1" : die("Некорретный тип банов.")));
+$cmd    = ($type == "0" ? "banid" : "addip");
+$field  = ($type == "0" ? "authid" : "ip");
+
+header('Content-Type: application/x-httpd-php php');
+header('Content-Disposition: attachment; filename="banned_' . (($type == "0") ? "user" : "ip") . '.cfg"');
+
+$bans = $GLOBALS['db']->getAll("SELECT authid FROM `:prefix:bans` WHERE length = '0' AND RemoveType IS NULL AND type = ?", array($type));
+foreach ($bans as $ban) {
+    print($cmd . " 0 " . $ban[$field] . "\r\n");
+}
